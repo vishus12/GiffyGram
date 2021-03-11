e7463e9b370dd52ff3fe6c3e52559c76a77a9da6
Vishal Trivedi <vishaltrivedi92@gmail.com>
2021-03-11 13:32:36 -0600

    more edits

--- src/scripts/data/DataManager.js
+++ src/scripts/data/DataManager.js
@@ -1,29 +1,49 @@
 export const getUsers = () => {
 
-    return fetch("http://localhost:8088/users")
+  return fetch("http://localhost:8088/users")
     .then(response => response.json())
-    // .then(parsedResponse => {
-    //     // do something with response here
-    //     return parsedResponse;
-    // })
+  // .then(parsedResponse => {
+  //     // do something with response here
+  //     return parsedResponse;
+  // })
+}
+
+let postCollection = [];
+
+export const usePostCollection = () => {
+  //Best practice: we don't want to alter the original state, so
+  //make a copy of it and then return it
+  //The spread operator makes this quick work
+  return [...postCollection];
 }
 
 export const getPosts = () => {
-
-    return fetch("http://localhost:8088/posts")
+  return fetch("http://localhost:8088/posts")
     .then(response => response.json())
-    // .then(parsedResponse => {
-    //     // do something with response here
-    //     return parsedResponse;
-    // })
+    .then(parsedResponse => {
+      postCollection = parsedResponse
+      return parsedResponse;
+    })
+}
+
+export const createPost = postObj => {
+  return fetch("http://localhost:8088/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(postObj)
+
+  })
+    .then(response => response.json())
 }
 
 const loggedInUser = {
-	id: 1,
-	name: "Bryan",
-	email: "bryan@bn.com"
+  id: 1,
+  name: "Bryan",
+  email: "bryan@bn.com"
 }
 
 export const getLoggedInUser = () => {
-	return loggedInUser;
+  return loggedInUser;
 }
--- src/scripts/footer.js
+++ src/scripts/footer.js
@@ -5,6 +5,7 @@ export const footer = () => {
         <footer class="footer">
             <div class="footer__item">
                 Posts since <select id="yearSelection">
+                    <option>2021</option>
                     <option>2020</option>
                     <option>2019</option>
                     <option>2018</option>
