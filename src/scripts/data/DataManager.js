31feca22c46d86c4f9fc760bc9fbd5e843c808c2
Vishal Trivedi <vishaltrivedi92@gmail.com>
2021-03-24 15:16:23 -0500

    USER FUNCTIONALITY, LOGIN/LOGOUT

--- src/scripts/data/DataManager.js
+++ src/scripts/data/DataManager.js
@@ -18,9 +18,11 @@ export const usePostCollection = () => {
 }
 
 export const getPosts = () => {
-  return fetch("http://localhost:8088/posts")
+  const userId = getLoggedInUser().id
+  return fetch(`http://localhost:8088/posts?_expand=user`)
     .then(response => response.json())
     .then(parsedResponse => {
+      console.log("data with user", parsedResponse)
       postCollection = parsedResponse
       return parsedResponse;
     })
@@ -68,12 +70,48 @@ export const updatePost = postObj => {
     .then(getPosts)
 }
 
-const loggedInUser = {
-  id: 1,
-  name: "Bryan",
-  email: "bryan@bn.com"
-}
+let loggedInUser = {}
 
 export const getLoggedInUser = () => {
-  return loggedInUser;
+  return { ...loggedInUser };
 }
+
+export const logoutUser = () => {
+  loggedInUser = {}
+}
+
+export const setLoggedInUser = (userObj) => {
+  loggedInUser = userObj;
+}
+
+export const loginUser = (userObj) => {
+  return fetch(`http://localhost:8088/users?name=${userObj.name}&email=${userObj.email}`)
+    .then(response => response.json())
+    .then(parsedUser => {
+      //is there a user?
+      console.log("parsedUser", parsedUser) //data is returned as an array
+      if (parsedUser.length > 0) {
+        setLoggedInUser(parsedUser[0]);
+        return getLoggedInUser();
+      } else {
+        //no user
+        return false;
+      }
+    })
+}
+
+export const registerUser = (userObj) => {
+  return fetch(`http://localhost:8088/users`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(userObj)
+  })
+    .then(response => response.json())
+    .then(parsedUser => {
+      setLoggedInUser(parsedUser);
+      return getLoggedInUser();
+    })
+}
+
--- src/scripts/feed/Post.js
+++ src/scripts/feed/Post.js
@@ -1,15 +1,21 @@
+import { getLoggedInUser } from "../data/DataManager.js"
+
 export const Post = (postObject) => {
-    return `
+  return `
       <section class="post">
         <header>
             <h2 class="post__title">${postObject.title}</h2>
         </header>
+        <p> posted by: ${postObject.user.name}</p>
         <img class="post__image" src="${postObject.imageURL}" />
         <userId: ${postObject.userId}</p>
         <p> description: ${postObject.description}</p>
         <p> timestamp: ${postObject.timestamp}</p>
-        <button id="edit__${postObject.id}">Edit</button>
-        <button id="delete__${postObject.id}">Delete</button>
+        ${postObject.user.id === getLoggedInUser().id
+      ? `<button id="edit__${postObject.id}">Edit</button>
+        <button id="delete__${postObject.id}">Delete</button>`
+      : ""
+    }
       </section>
     `
-  }
+}
